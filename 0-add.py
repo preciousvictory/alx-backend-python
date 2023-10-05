44c872338ce781793dda0464b3acb020dc70160a
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-05 06:32:18 -0700

    typed annotated function

--- 0-add.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""0-add"""
-
-
-def add(a: float, b: float) -> float:
-    return a + b
--- 1-concat.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""1-concat"""
-
-
-def concat(str1: str, str2: str) -> str:
-    return "{}{}".format(str1, str2)
