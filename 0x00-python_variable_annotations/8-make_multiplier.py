79a1ae00a5c3cb0ba252a130e4067f5fd7c8fc52
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-05 16:08:15 -0700

    type-annotated function that takes a float  multiplier as argument and returns a function that multiplies a float by multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''8-make_multiplier
+'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''make_multiplier
+    '''
+    return lambda x: x * multiplier
