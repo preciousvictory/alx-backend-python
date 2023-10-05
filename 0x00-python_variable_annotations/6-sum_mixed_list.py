9a8eeed5097fe92ccc39e2419ca94526c5806826
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-05 07:44:58 -0700

    type-annotated function  which takes a list of integers and floats

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''6-sum_mixed_list
+'''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int,  float]]) -> float:
+    '''sum_mixed_list
+    '''
+    return sum(mxd_lst)
