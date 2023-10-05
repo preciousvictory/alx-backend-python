0ee0202bea15080fdaa75b6e43fa888405a510c8
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-05 16:27:28 -0700

    Iterable, Sequence, Tuple

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''9-element_length
 '''
-from typing import Tuple, Sequence, Iterable
+from typing import Tuple, Sequence, Iterable, List
 
 
-def element_length(lst: Iterable[Sequence]) -> Tuple[Sequence, int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''element_length
     '''
     return [(i, len(i)) for i in lst]
