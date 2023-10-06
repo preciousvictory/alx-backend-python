6639fe216b53d5854bb28ffa4ec7d0f321d67af2
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-05 17:05:45 -0700

    Any, TypeVar - type annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''100-safe_first_element
+'''
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''safe_first_element
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''101-safely_get_value
+'''
+from typing import Mapping, Any, Union, TypeVar
+
+
+T = TypeVar('T')
+dft = Union[T, None]
+Rtn = Union[Any, T]
+
+def safely_get_value(dct: Mapping, key: Any, default: dft = None) -> Rtn:
+    '''safely_get_value
+    '''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
