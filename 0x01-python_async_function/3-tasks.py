d698c6c6057207de1601ec723a96f9ccac400e2f
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-09 19:29:54 -0700

    asynio.task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,5 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''task_wait_random asyncio.task'''
     task = asyncio.create_task(wait_random(max_delay))
     return task
