04b3b28e6941bd893432a4edc7c1adee1b0e276b
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-09 18:08:43 -0700

    asyncio python

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 '''0-basic_async_syntax
 '''
-from random import uniform
+import random
 import asyncio
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay:int = 10) -> float:
     '''wait_random
     '''
-    delay = uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
