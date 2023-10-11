711f75d00971a8bcbef84e0e20bfc4ebee81042f
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-10 18:06:57 -0700

    measure run time

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,9 +2,17 @@
 '''2-measure_runtime
 '''
 import asyncio
+import time
 
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-k
+async def measure_runtime() -> float:
+    '''measure_runtime
+    '''
+    start_time = time.time()
+    coros = [async_comprehension() for _ in range(4)]
+    data = await asyncio.gather(*coros)
+
+    return (time.time() - start_time)
