d6c056d94be3ffba1d3d5bb11154aa30de176fa8
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-10 17:48:20 -0700

    asyn comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 '''1-async_comprehension'''
 import asyncio
-from typing import Generator
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     '''async_comprehension
     '''
     return [i async for i in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''2-measure_runtime
+'''
+import asyncio
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+k
