0be805b87529e70ac67e6970ea5342822a1bcf78
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-29 20:50:24 -0700

    unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,18 @@
 """TestAccessNestedMap
 """
 import unittest
+from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ unit test for utils.access_nested_map
     """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """method to test that the method access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
---
+++ 0x03-Unittests_and_integration_tests/utils_main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+access_nested_map = __import__('utils').access_nested_map
+
+print(access_nested_map(nested_map={"a": 1}, path=("a",)))
+print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a",)))
+print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a", "b")))
