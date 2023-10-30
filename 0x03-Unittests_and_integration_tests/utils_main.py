3c54e04bf9811a616eda2e1bd9ecc68cd5ec8378
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-29 21:01:50 -0700

    unittest

--- 0x03-Unittests_and_integration_tests/utils_main.py
+++ 0x03-Unittests_and_integration_tests/utils_main.py
@@ -5,3 +5,6 @@ access_nested_map = __import__('utils').access_nested_map
 print(access_nested_map(nested_map={"a": 1}, path=("a",)))
 print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a",)))
 print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a", "b")))
+
+print(access_nested_map(nested_map={}, path=("a",)))
+print(access_nested_map(nested_map={"a": 1}, path=("a", "b")))
